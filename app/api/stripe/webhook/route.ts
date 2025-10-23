31d76cd5e8ee4340e853ac0da1cc306d9e1c7c11
Adelerln <adele.rolin@gmail.com>
2025-10-23 14:37:14 +0200

    fix pour deploy

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -72,4 +70,3 @@ export async function POST(request: Request) {
 
   return NextResponse.json({ received: true }, { status: 200 });
 }
-
